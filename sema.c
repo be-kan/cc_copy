286646ebf4a06f61d6acba0312762db85898b63e
神戸隆太 <kambe3141@gmail.com>
2018-11-21 13:16:06 +0900

    move lval check to sema.c

--- sema.c
+++ sema.c
@@ -80,6 +80,9 @@ static Node *walk(Node *node, bool decay) {
             return node;
         case '=':
             node->lhs = walk(node->lhs, false);
+            if (node->lhs->op != ND_LVAR && node->lhs->op != ND_DEREF) {
+                error("not an lvalue: %d (%s)", node->op, node->name);
+            }
             node->rhs = walk(node->rhs, true);
             node->ty = node->lhs->ty;
             return node;
@@ -110,7 +113,7 @@ static Node *walk(Node *node, bool decay) {
             Node *expr = walk(node->expr, false);
             Node *ret = calloc(1, sizeof(Node));
             ret->op = ND_NUM;
-            ret->ty = INT;
+            ret->ty = INT; // warning
             ret->val = size_of(expr->ty);
             return ret;
         }
