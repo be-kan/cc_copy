da771c0e1e29ce1d3f1fd0d55f43bb916cc04b4b
神戸隆太 <kambe3141@gmail.com>
2018-11-22 16:02:43 +0900

    fix array operator

--- test.sh
+++ test.sh
@@ -101,7 +101,7 @@ try 2 'int _main() { int **p = _alloc_ptr_ptr(2); return **p; }'
 try 3 'int _main() { int ary[2]; *ary=1; *(ary+1)=2; return *ary + *(ary+1); }'
 try 5 'int _main() { int x; int *p = &x; x = 5; return *p; }'
 
-try 3 'int _main() { int ary[2]; ary[0]=1; ary[1]=2; return ary[0] + ary[1];}'
+try 3 'int _main() { int ary[2]; ary[0]=1; ary[1]=2; return ary[0] + ary[0+1];}'
 try 5 'int _main() { int x; int *p = &x; x = 5; return p[0];}'
 
 try 1 'int _main() { char x; return sizeof x; }'
