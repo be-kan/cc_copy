53a6ceabbc50bf738822c460a8f505cbac24ee29
神戸隆太 <kambe3141@gmail.com>
2018-12-09 14:21:28 +0900

    emit .bss section for sero-initialized data

--- parse.c
+++ parse.c
@@ -797,9 +797,8 @@ static void toplevel() {
     }
 
     ty->is_extern = is_extern;
-    char *data = calloc(1, ty->size);
     int len = ty->size;
-    add_gvar(ty, name, data, len);
+    add_gvar(ty, name, NULL, len);
 }
 
 static bool is_eof() {
